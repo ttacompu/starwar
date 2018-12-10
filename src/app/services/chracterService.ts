d4865d2568535f7455c840d25f3faed69bd945e3
ttacompu@gmail.com <taung@cgsh.com>
2018-12-10 12:21:53 -0500

    sorted

--- src/app/services/chracterService.ts
+++ src/app/services/chracterService.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { mergeMap, map, tap, catchError } from "rxjs/operators";
 import { forkJoin, of, throwError } from "rxjs";
+import * as R from 'ramda';
 
 @Injectable()
 export class ChracterService {
@@ -32,12 +33,14 @@ export class ChracterService {
     getFirmsUrl(url) {
       return this.http_.get(url).pipe(map((x:any)=> x.films), mergeMap(urls =>{
         const requests = urls.map(x => this.http_.get(x));
-      return forkJoin(requests).pipe(map((results:any)=> results.map(x=>{
+      return forkJoin(requests).pipe(map((results:any)=> R.sortBy(R.prop('release_date'), results.map(x=>{
         return {
           ...x,
           release_date : new Date(x.release_date)
         }
-      }) ))
+      }) )
+      
+      ))
 
       }), catchError(err =>{
         return throwError(err)
