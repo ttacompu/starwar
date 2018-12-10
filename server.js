06fedc04a07bca8a668b1c2c9e36444548633168
ttacompu@gmail.com <taung@cgsh.com>
2018-12-10 10:41:09 -0500

    changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ClientApp'));
+app.use(express.static('./dist/ClientApp/assets'));
 
 app.get('/*', function(req,res) {
     
